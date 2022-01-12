1a36a7cc7d26c397ee02a7d5d551f57b155f9315
BIT <BIT@DESKTOP-28SDCPC>
2022-01-12 17:05:07 +0900

    prob1 commit

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -7,7 +7,14 @@ public class Prob1 {
 		Scanner scanner = new Scanner( System.in );
 		
 		/* 코드 작성 */
-		
+		System.out.print("수를 입력하시오: ");
+		int a=scanner.nextInt();
+		if(a%3==0) {
+			System.out.println("3의 배수입니다. ");
+		}
+		else {
+			System.out.println("3의 배수가 아닙니다");
+		}
 		scanner.close();
 	}
 }
