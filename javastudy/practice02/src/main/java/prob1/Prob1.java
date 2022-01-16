66f5c5b3ef8807ab424688dcd9044d11a2f2076e
김소원 <@DESKTOP-H6F59B9>
2022-01-16 21:52:01 +0900

    practice2-prob2 수정

--- javastudy/practice02/src/main/java/prob1/Prob1.java
+++ javastudy/practice02/src/main/java/prob1/Prob1.java
@@ -1,21 +1,20 @@
-package prob01;
+package prob1;
 
 import java.util.Scanner;
 
-public class Prob01 {
+public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in  );
 
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 		int arr[]=new int[10];
-		
+
+		System.out.print("금액: ");
 		int num = scanner.nextInt();
-		System.out.println("금액: "+num);
-		
 		for(int i=0; i<10; i++) {
 			arr[i]=num/MONEYS[i]; //갯수(몫)
-			money=num%MONEYS[i]; //나머지
-			System.out.println(MONEYS[i]+"원 권"+arr[i]+"개");
+			num=num%MONEYS[i]; //나머지
+			System.out.println(MONEYS[i]+"원권 "+arr[i]+"개");
 		}
 		
 		scanner.close();
