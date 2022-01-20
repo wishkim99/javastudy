43dbaa2a37821ee1273bc23c6ac1a3b2df8e460e
김소원 <@DESKTOP-H6F59B9>
2022-01-20 20:52:41 +0900

    practice04-prob3

---
+++ javastudy/practice04/src/main/java/prob3/DropShip.java
@@ -0,0 +1,16 @@
+package prob3;
+
+public class DropShip extends Unit{
+	// 현재 위치
+	private int x;
+	private int y;
+
+	
+	void load() {
+		/* 선택된 대상을 태운다.*/ 
+	}
+	
+	void unload() {
+		/* 선택된 대상을 내린다.*/ 
+	}
+}
---
+++ javastudy/practice04/src/main/java/prob3/Marine.java
@@ -0,0 +1,11 @@
+package prob3;
+
+public class Marine extends Unit{
+	// 현재 위치
+	private int x;
+	private int y;
+
+	void stimPack() { 
+		/* 스팀팩을 사용한다.*/
+	}	
+}
