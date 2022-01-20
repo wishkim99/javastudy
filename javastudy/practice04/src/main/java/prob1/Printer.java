0b5277c5cef81766eb3da95c3c043ed29c1d11dd
김소원 <@DESKTOP-H6F59B9>
2022-01-20 09:29:27 +0900

    practice04-prob1

---
+++ javastudy/practice04/src/main/java/prob1/Printer.java
@@ -0,0 +1,21 @@
+package prob1;
+
+public class Printer {
+
+	public void println(int i) {
+		System.out.println(i);
+	}
+
+	public void println(boolean bool) {
+		System.out.println(bool);
+	}
+
+	public void println(double d) {
+		System.out.println(d);
+	}
+
+	public void println(String s) {
+		System.out.println(s);
+	}
+
+}
